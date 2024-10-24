0be5833cc56a29bc40c5f975dc5fe5e4befa7c47
CamilaTovarm <mctovar@ucundinamarca.edu.co>
2024-10-24 13:11:29 -0500

    Modelos

---
+++ WeatherStation/Models/Location.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherStation.Models
+{
+    public class Location
+    {
+        public int IdLocation { get; set; }
+        public string Latitude {  get; set; }
+        public string Longitude { get; set; }
+        public DateTime CreateDate { get; set; }
+        public bool State { get; set; }
+    }
+}
---
+++ WeatherStation/Models/Users.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherStation.Models
+{
+    public class Users
+    {
+        public int IdUser { get; set; }
+        public int PersonId { get; set; }
+        public int Username { get; set; }
+        public DateTime CreateDate { get; set; }
+        public bool State { get; set; }
+    }
+}
