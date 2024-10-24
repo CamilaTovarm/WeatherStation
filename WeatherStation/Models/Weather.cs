44ce3764303854b62268ee588f1c84de3594b48b
CamilaTovarm <mctovar@ucundinamarca.edu.co>
2024-10-24 15:34:39 -0500

    Modelos, DbContext, Program

--- WeatherStation/Models/Weather.cs
+++ WeatherStation/Models/Weather.cs
@@ -3,11 +3,6 @@
     public class Weather
     {
         public int IdWeather { get; set; }
-        public int IdLocation { get; set; }
-        public int IdUser { get; set; }
-        public string Humidity { get; set; }
-        public string Temperature { get; set; }
-        public string AmosphericPressure { get; set; }
         public DateTime CreateDate { get; set; }
         public bool State { get; set; }
     }
